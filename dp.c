75ac01e8370f404900762bce236b8acd24a8fadd
DEEPIMA <deepudeepi.324@gmail.com>
2016-10-20 14:01:00 +0530

    Add files via upload

---
+++ dp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+   int n;
+   
+ printf("Enter the number");
+   
+ scanf("%d",&n);
+   
+ if(n>0)
+   
+ printf("Positive");
+   
+ else
+   
+ if(n<0)
+   
+ printf("Negative");
+
+else
+printf("zero");
+ 
+   
+}
